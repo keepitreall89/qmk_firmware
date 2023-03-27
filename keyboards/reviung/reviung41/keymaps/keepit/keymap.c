933ec45b3ffc5197f296c19ff7b1eb58703bac1e
keepitreall89 <31910114+keepitreall89@users.noreply.github.com>
2023-03-27 00:31:52 -0400

    Adding Rev41 keymap

--- keyboards/reviung/reviung41/keymaps/keepit/keymap.c
+++ keyboards/reviung/reviung41/keymaps/keepit/keymap.c
@@ -28,32 +28,32 @@ enum layer_names {
 #define KC_CAD LALT(LCTL(KC_DEL))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_reviung39(
+  [_BASE] = LAYOUT_reviung41(
     KC_BSPC,   	KC_QUOT,	KC_COMM,	KC_DOT,	KC_P,	KC_Y,		KC_F,	KC_G,	KC_C,	KC_R,	KC_L,	KC_BSPC,
     KC_TAB,  	KC_A,     	KC_O,     	KC_E,	KC_U,	KC_I,		KC_D,	KC_H,	KC_T,	KC_N,	KC_S,  	KC_TAB,
     KC_LCTL,  	KC_SCLN,    KC_Q,     	KC_J,   KC_K,   KC_X,   	KC_B,   KC_M,   KC_W,  	KC_V,   KC_Z,  	KC_ENT,
-                                                      LOWER,    KC_SPC,   RAISE
+                                                     KC_LALT, LOWER,    KC_SPC,   RAISE, LOWER
   ),
   
-  [_LOWER] = LAYOUT_reviung39(
+  [_LOWER] = LAYOUT_reviung41(
     KC_ESC,  KC_GRV,  KC_NO,      KC_NO,      KC_CAD,     LGUI(KC_L), 		KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,  	KC_DEL,
     KC_MINS, KC_ASTR, KC_SLASH,	  KC_LBRC,	  KC_LPRN,	  KC_LCBR,	  		KC_AT,   KC_QUES, KC_HASH, KC_EQL,  KC_AMPR,	KC_NO,
     KC_LSFT, OSM(MOD_LGUI),	  LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,      		KC_NO,   KC_NO,	  KC_NO,   KC_NO,	KC_NO,	KC_NO,
-                                                       _______,  KC_SPC,   _______
+                                                     _______,  _______,  KC_SPC,   _______, _______
   ),
   
-  [_RAISE] = LAYOUT_reviung39(
+  [_RAISE] = LAYOUT_reviung41(
     KC_NO,  		KC_HOME,   		KC_UP,     		KC_END,	KC_NO, LGUI(KC_L),      KC_NUM,  KC_PSLS, KC_PMNS, KC_P7, KC_P8, KC_P9,
     KC_NO,  		KC_LEFT,  		KC_DOWN,   		KC_RGHT,KC_NO, KC_NO,      KC_NO,   KC_PAST, KC_PPLS, KC_P4, KC_P5, KC_P6,
     OSM(MOD_LCTL),  OSM(MOD_LALT),  KC_NO,  KC_NO,  KC_NO, KC_NO,      KC_PEQL, KC_PDOT, KC_P0,   KC_P1, KC_P2, KC_P3,
-														_______,  KC_SPC,  _______
+													_______, _______,  KC_SPC,  _______, _______
   ),
   
-  [_ADJUST] = LAYOUT_reviung39(
+  [_ADJUST] = LAYOUT_reviung41(
     RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
     RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 	KC_NO  ,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            KC_NO,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                                       _______,  XXXXXXX,  _______
+                                                      KC_NO, _______,  XXXXXXX,  _______, _______
   ),
 };
 
